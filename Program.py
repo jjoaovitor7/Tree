2553e4a193144a9231df558e3f5f35d4b9346cea
João Vítor <joaovitorsf7@hotmail.com>
2022-02-25 05:59:48 -0300

    implementação anterior de árvore descartada.

--- Program.py
+++ Program.py
@@ -1,134 +1,97 @@
-from sys import getsizeof
-from Tree import Tree
+nodesLeaf = []
 
-def count_keys(dict_, counter=0):
-    for each_key in dict_:
-        if isinstance(dict_[each_key], dict):
-            counter = count_keys(dict_[each_key], counter + 1)
-        else:
-            counter += 1
-    return counter
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+        self.degree = 0
+        self.nodesLeaf = []
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+        self.degree = len(self.children)
+
+    def get_level(self):
+        level = 0
+        p = self.parent
+
+        while p:
+            level += 1
+            p = p.parent
+        return level
+
+    def preorder(self):
+        spaces = " " * self.get_level()
+        prefix = spaces + "|_" if self.parent != None else ""
+
+        if (self.degree == 0):
+            nodesLeaf.append(self.data)
+
+        print(f"{prefix} {self.data} {self.get_level()} {self.degree}")
+        if (len(self.children) > 0):
+            for child in self.children:
+                child.preorder()
+
+    def get_nodesLeaf(self):
+        return self.nodesLeaf
 
 
-class Program():
+def add__recursive(nA, nB):
+    for k in nA.children:
+        nFSubSub = input(f"Nó Filho de {k.data}\n:")
+        if (nFSubSub == "-1"):
+            break
+        global nFSubSubNode
+        nFSubSubNode = Node(nFSubSub)
+        nB.add_child(nFSubSubNode)
+
+    if (len(nA.children) != 0):
+        return
+    else:
+        add__recursive(nFSubSubNode, nA)
+
+
+def Tree():
+    root = Node(input("Nó Raiz\n:"))
+
+    nn = None
+    while True:
+        n = input("Nó Filho\n:")
+        if (n == "-1"):
+            break
+        nn = Node(n)
+
+        for i in root.children:
+            nF = input(f"Nó Filho de {i.data}\n:")
+            if (nF == "-1"):
+                break
+            nFNode = Node(nF)
+            nn.add_child(nFNode)
+            for j in nn.children:
+                nFSub = input(f"Nó Filho de {j.data}\n:")
+                if (nFSub == "-1"):
+                    break
+                nFSubNode = Node(nFSub)
+                nFNode.add_child(nFSubNode)
+                add__recursive(nFNode, nFSubNode)
+
+        root.add_child(nn)
+
+    return root
+
+
+class Program:
     def __init__(self):
         pass
 
     def run(self):
-        _tree = dict()
-        tree = Tree(_tree)
-        exec = True
+        Tree().preorder()
 
-        while (exec):
-            opt = int(input("""Opções
- 1: Adicionar Nó
- 2: Visualizar Nós
- 3: Visualizar Níveis
- 4: Visualizar Graus
- 5: Visualizar Nós Folhas
- 6: Visualizar Representação Gráfica
- 7: Visualizar Subárvore de Nó
-99: Sair
-:"""))
-
-            if (opt == 1):
-                while(True):
-                    node = input("Nó:")
-
-                    if (node == ""):
-                        node = None
-
-                    if (node != "-1"):
-                        if (len(_tree) == 0):
-                            _tree[node] = {}
-                        else:
-                            def add(d, l):
-                                if (isinstance(d, str) and not(node in list(l))):
-                                    c = list(l.copy())
-                                    global p
-                                    if (count_keys(l[c[0]]) == 2):
-                                        p = c.pop(0)
-
-                                    if (count_keys(l[c[0]]) < 2):
-                                        l[c[0]][node] = {}
-                                    else:
-                                        if (p != None):
-                                            _iter = iter(l[p])
-
-                                            while (True):
-                                                try:
-                                                    l[p][next(_iter)][node] = {}
-                                                    break
-                                                except StopIteration:
-                                                    add(node, l[p])
-                                                    break
-
-                                else:
-                                    if (l != None and node in list(l)):
-                                        pass
-                                    else:
-                                        for key, value in d.items():
-                                            if (isinstance(value, dict)):
-                                                if (count_keys(value) < 2):
-                                                    if (None in value):
-                                                        del value[None]
-                                                    value[node] = {}
-
-                                                else:
-                                                    if (None in value):
-                                                        del value[None]
-                                                        value[node] = {}
-                                                    else:
-                                                        add(node, value)
-                            add(_tree, None)
-
-                    if (node == "-1"):
-                        break
-
-            if (opt == 2):
-                print("Nós:")
-                nodes = tree.getNodes()
-                for i in nodes:
-                    print(i, end=" ")
-
-            elif (opt == 3):
-                print("\nNíveis:")
-                levels = tree.getLevels()
-                for i in range(len(levels)):
-                    for j in levels[i]:
-                        print(j, end=" ")
-                    print("|", end=" ")
-
-            elif (opt == 4):
-                print("\nGraus:")
-                degrees = tree.getDegrees()
-                for i in range(len(degrees)):
-                    for j in degrees[i]:
-                        print(j, end=" ")
-                    print("|", end=" ")
-
-            elif (opt == 5):
-                print("\nNós Folhas:")
-                for i in tree.getLeafNodes():
-                    print(i, end=" ")
-
-            elif (opt == 6):
-                try:
-                    tree.generateGraph()
-                except:
-                    pass
-
-            elif (opt == 7):
-                node = input("Nó:")
-                print(tree.getSubTree(node))
-
-            elif (opt == 99):
-                break
-
-            else:
-                print("Opção não encontrada.")
-
-            print("\n")
+        print("Nós Folhas:")
+        for i in nodesLeaf:
+            print(i, end=" ")
 
 
 if __name__ == "__main__":
